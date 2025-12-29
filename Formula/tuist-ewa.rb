19c8fb646f486acf5f32acaac65008bb1d0ad64d
Anton Aleshkevich <sc0rch@me.com>
2025-12-29 21:56:11 +0300

    tuist-ewa 4.118.1-ewa.1

--- Formula/tuist-ewa.rb
+++ Formula/tuist-ewa.rb
@@ -3,66 +3,26 @@
 require "pathname"
 
 class TuistEwa < Formula
-  desc "Tuist fork with skipPackageResolution flag"
-  homepage "https://github.com/sc0rch/tuist"
-  url "https://github.com/sc0rch/tuist/archive/refs/heads/homebrew-4.116.1.tar.gz"
-  version "4.116.1-ewa"
-  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+  desc "Tuist fork"
+  homepage "https://github.com/sc0rch/tuist-ewa"
+  url "https://github.com/sc0rch/tuist-ewa/releases/download/4.118.1-ewa.1/tuist.zip"
+  version "4.118.1-ewa.1"
+  sha256 "438381a21d7756846bfcec04668ded2a324cd26116315e3a911b29ce68f4fffa"
   license "Apache-2.0"
 
-  depends_on xcode: ["15.0", :build]
-
   def install
-    ENV["SWIFT_BUILD_ENABLE_SANDBOX"] = "0"
-    scratch = buildpath/"swift-build"
-    base_args = [
-      "swift", "build",
-      "--disable-sandbox",
-      "--replace-scm-with-registry",
-      "--scratch-path", scratch,
-      "-c", "release",
-    ]
-    system(*(base_args + ["--product", "tuist"]))
+    bin.install "tuist"
 
-    %w[ProjectDescription ProjectAutomation].each do |product|
-      system(*(base_args + ["--product", product]))
-    end
+    lib.install "ProjectDescription.framework"
 
-    build_products = Pathname.new(
-      Utils.safe_popen_read(
-        "swift", "build",
-        "--disable-sandbox",
-        "--replace-scm-with-registry",
-        "--scratch-path", scratch,
-        "--show-bin-path",
-        "-c", "release"
-      ).strip
-    )
+    share.install "Templates"
 
-    bin.install build_products/"tuist"
-
-    architecture_output = Pathname.glob(scratch/"*-apple-macosx/release").first
-    odie "Unable to locate release build output in #{scratch}" unless architecture_output
-
-    frameworks_path = libexec/"Frameworks"
-    frameworks_path.mkpath
-
-    {
-      "ProjectDescription" => %w[swiftmodule swiftdoc swiftsourceinfo abi.json],
-      "ProjectAutomation" => %w[swiftmodule swiftdoc swiftsourceinfo abi.json],
-    }.each do |product, extensions|
-      dylib_path = architecture_output/"lib#{product}.dylib"
-      cp dylib_path, frameworks_path
-
-      modules_dir = architecture_output/"Modules"
-      Pathname.glob("#{modules_dir}/#{product}.{#{extensions.join(",")}}").each do |module_file|
-        cp module_file, frameworks_path
-      end
+    if Pathname("vendor").exist?
+      libexec.install "vendor"
     end
 
     (bin/"tuist-ewa").write <<~EOS
       #!/bin/bash
-      export TUIST_FRAMEWORK_SEARCH_PATHS="#{frameworks_path}"
       exec "#{bin}/tuist" "$@"
     EOS
     chmod 0755, bin/"tuist-ewa"
